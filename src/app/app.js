31d8e33eac6efe9c3b4fd125c8ac77e1df62205b
Gustavo Duarte <gduartemenezes@gmail.com>
2020-10-06 16:11:52 -0300

    cria  app index

---
+++ src/app/app.js
@@ -0,0 +1,13 @@
+class Application {
+  constructor({ db, server }) {
+    this.database = db;
+    this.server = server;
+  }
+
+  async start(container) {
+    await this.database.connect();
+    await this.server.start(container);
+  }
+}
+
+export default new Application();
